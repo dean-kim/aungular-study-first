b85bdbc1da85f051ec31cb6a406cf7b4afbfb8fe
dean-kim <keunu07@gmail.com>
2017-06-16 10:16:42 +0900

    hello component connect

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.css']
 })
-export class HelloComponent implements OnInit {
+export class HelloComponent  {
+  name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  setName(name: string) {
+    this.name = name;
   }
-
 }
